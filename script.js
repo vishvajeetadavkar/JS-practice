b5ff4ac07ed000177f609c9eb3c38e2e7c472553
vishvajeet adavkar <vishvajeetadavkar@gmail.com>
2024-11-27 23:35:21 +0530

    prototypal inheritance, JS Closures

--- script.js
+++ script.js
@@ -1,59 +1,80 @@
-var a= 10;
-var b=20;
-prompt("hey");
+// var a= 10;
+// var b=20;
+// prompt("hey");
 
-// use of this in different cases 
+// // use of this in different cases 
 
-//global -> window
-console.log(this);
+// //global -> window
+// console.log(this);
 
-//function -> window
+// //function -> window
+// function abc(){
+//     console.log(this);
+// }
+// abc();
+
+// // method -> obj
+// var obj = {
+//     name: function(){
+//         console.log(this);
+//     },
+//     age: 24
+// }
+// obj.name();
+
+// // method -> any func. inside an object
+
+// //function inside method (es5) -> window
+// var obj2 = {
+//     name2: function(){
+//         function child(){
+//             console.log(this);
+//         }
+//         child();
+//     }
+// }
+// obj2.name2();
+
+// //function inside method (es6) -> obj.
+// var obj3 = {
+//     name3: function(){
+//         child=() =>{
+//             console.log(this);
+//     }
+//     child();
+// }
+// }
+// obj3.name3();
+
+// // constructor func. -> new blank obj.
+// function add(){
+//     console.log(this);
+// }
+// const ans= new add();
+
+// // event listener - that element on which event listener is applied
+// // document.querySelector("button")
+// // .addEventListener("click", function(){
+// //     console.log(this);
+// // })
+
+// prototypal inheritance
+function makeHuman(name, age){
+    this.name=name;
+    this.age=age;
+}
+makeHuman.prototype.printName=function(){
+    console.log(this.name);
+}
+const human1=new makeHuman("harsh",25);
+const human2=new makeHuman("harshita",35);
+
+//JS Closures
 function abc(){
-    console.log(this);
-}
-abc();
-
-// method -> obj
-var obj = {
-    name: function(){
-        console.log(this);
-    },
-    age: 24
-}
-obj.name();
-
-// method -> any func. inside an object
-
-//function inside method (es5) -> window
-var obj2 = {
-    name2: function(){
-        function child(){
-            console.log(this);
-        }
-        child();
+    var a=12;
+    return function(){
+        console.log(a);
     }
 }
-obj2.name2();
-
-//function inside method (es6) -> obj.
-var obj3 = {
-    name3: function(){
-        child=() =>{
-            console.log(this);
-    }
-    child();
-}
-}
-obj3.name3();
-
-// constructor func. -> new blank obj.
-function add(){
-    console.log(this);
-}
-const ans= new add();
-
-// event listener - that element on which event listener is applied
-document.querySelector("button")
-.addEventListener("click", function(){
-    console.log(this);
-})
+var ans=abc();
+ans();
